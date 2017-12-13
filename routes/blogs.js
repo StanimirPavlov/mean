6d444f9a0481402c0cec527add88a6545feca8c4
StanimirPavlov <stanimir.pavlov@abv.bg>
2017-12-14 00:49:25 +0200

    Edit posts

--- routes/blogs.js
+++ routes/blogs.js
@@ -56,7 +56,84 @@ module.exports = (router) => {
           }
         }
       }).sort({'_id': -1});
-    })
+    });
 
+    router.get('/singleBlog/:id', (req, res) => {
+      if (!req.params.id) {
+        res.json({ success: false, message: 'No blog ID was provided.' });
+      } else {
+        Blog.findOne({ _id: req.params.id }, (err, blog) => {
+          if (err) {
+            res.json({ success: false, message: 'Not a valid blog id' });
+          } else {
+            if (!blog) {
+              res.json({ success: false, message: 'Blog not found.' });
+            } else {
+              User.findOne({ _id: req.decoded.userId }, (err, user) => {
+                if (err) {
+                  res.json({ success: false, message: err });
+                } else {
+                  if (!user) {
+                    res.json({ success: false, message: 'Unable to authenticate user' });
+                  } else {
+                    if (user.username !== blog.createdBy) {
+                      res.json({ success: false, message: 'You are not authorized to edit this blog.' });
+                    } else {
+                      res.json({ success: true, blog: blog });
+                    }
+                  }
+                }
+              });
+            }
+          }
+        });
+      }
+    });
+
+    router.put('/updateBlog', (req, res) => {
+      if (!req.body._id) {
+        res.json({ success: false, message: 'No blog id provided' });
+      } else {
+        Blog.findOne({ _id: req.body._id }, (err, blog) => {
+          if (err) {
+            res.json({ success: false, message: 'Not a valid blog id' });
+          } else {
+            if (!blog) {
+              res.json({ success: false, message: 'Blog id was not found.' });
+            } else {
+              User.findOne({ _id: req.decoded.userId }, (err, user) => {
+                if (err) {
+                  res.json({ success: false, message: err });
+                } else {
+                  if (!user) {
+                    res.json({ success: false, message: 'Unable to authenticate user.' });
+                  } else {
+                    if (user.username !== blog.createdBy) {
+                      res.json({ success: false, message: 'You are not authorized to edit this blog post.' }); 
+                    } else {
+                      blog.title = req.body.title;
+                      blog.body = req.body.body; 
+                      blog.save((err) => {
+                        if (err) {
+                          if (err.errors) {
+                            res.json({ success: false, message: 'Please ensure form is filled out properly' });
+                          } else {
+                            res.json({ success: false, message: err }); 
+                          }
+                        } else {
+                          res.json({ success: true, message: 'Blog Updated!' });
+                        }
+                      });
+                    }
+                  }
+                }
+              });
+            }
+          }
+        });
+      }
+    });
+
+  
     return router;
 };
