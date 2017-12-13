89cf3ce823f712a7dbaee088e8f47d2425d0c889
StanimirPavlov <stanimir.pavlov@abv.bg>
2017-12-13 18:52:14 +0200

    Create posts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component'
 import { ProfileComponent } from './components/profile/profile.component'
+import { BlogComponent } from './components/blog/blog.component';
 import { AuthGuard } from './guards/auth.guard';
 import { NotAuthGuard } from './guards/notAuth.guard';
-
 
 const appRoutes: Routes = [
     {
@@ -32,6 +32,11 @@ const appRoutes: Routes = [
     {
         path: 'profile',
         component: ProfileComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'blog',
+        component: BlogComponent,
         canActivate: [AuthGuard]
     },
     {
--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { tokenNotExpired } from 'angular2-jwt';
 @Injectable()
 export class AuthService {
 
-domain = "http://localhost:2121";
+domain = "http://localhost:2121/";
 authToken;
 user;
 options;
@@ -31,20 +31,20 @@ options;
   }
 
   registerUser(user){
-    return this.http.post(this.domain + '/authentication/register', user)
+    return this.http.post(this.domain + 'authentication/register', user)
     .map(res=>res.json());
   }
 
   checkUsername(username) {
-    return this.http.get(this.domain + '/authentication/checkUsername/' + username).map(res => res.json());
+    return this.http.get(this.domain + 'authentication/checkUsername/' + username).map(res => res.json());
   }
 
   checkEmail(email) {
-    return this.http.get(this.domain + '/authentication/checkEmail/' + email).map(res => res.json());
+    return this.http.get(this.domain + 'authentication/checkEmail/' + email).map(res => res.json());
   }
 
   login(user) {
-    return this.http.post(this.domain + '/authentication/login', user).map(res => res.json());
+    return this.http.post(this.domain + 'authentication/login', user).map(res => res.json());
   }
 
   logout(){
@@ -62,7 +62,7 @@ options;
 
   getProfile(){
     this.createAuthenticationHeaders();
-    return this.http.get(this.domain + '/authentication/profile', this.options).map(res => res.json());  
+    return this.http.get(this.domain + 'authentication/profile', this.options).map(res => res.json());  
   }
 
   loggedIn(){
