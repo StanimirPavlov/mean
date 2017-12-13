de5a0f0e250d163b518c4dfb85943caa94aa2214
StanimirPavlov <stanimir.pavlov@abv.bg>
2017-12-13 04:07:02 +0200

    HTTP for registration, login - headers and jwt

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms'
@@ -9,6 +8,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 
 @NgModule({
@@ -17,15 +20,18 @@ import { RegisterComponent } from './components/register/register.component';
     NavbarComponent,
     HomeComponent,
     DashboardComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { setTimeout } from 'timers';
+
 
 @Component({
   selector: 'app-register',
@@ -9,8 +13,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
 form: FormGroup;
+message;
+messageClass;
+processing = false;
+emailValid;
+emailMessage;
+usernameValid;
+usernameMessage;
 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) { 
     this.createForm()
   }
 
@@ -36,6 +51,20 @@ form: FormGroup;
       ])],
       confirm: ['', Validators.required]
     }, { validator: this.matchingPasswords('password', 'confirm')})
+  }
+
+  disableForm(){
+    this.form.controls['email'].disable();
+    this.form.controls['username'].disable();
+    this.form.controls['password'].disable();
+    this.form.controls['confirm'].disable();    
+  }
+
+  enableForm(){
+    this.form.controls['email'].enable();
+    this.form.controls['username'].enable();
+    this.form.controls['password'].enable();
+    this.form.controls['confirm'].enable();
   }
 
   validateEmail(controls){
@@ -78,7 +107,52 @@ form: FormGroup;
 
 
   onRegisterSubmit(){
-    console.log('Form submitted!');
+    this.processing = true;
+    this.disableForm();
+    const user = {
+      email: this.form.get('email').value,
+      username: this.form.get('username').value,
+      password: this.form.get('password').value
+    }
+
+    this.authService.registerUser(user).subscribe(data=>{
+      if(!data.success){
+        this.messageClass = 'alert alert-danger';
+        this.message = data.message;
+        this.processing = false;
+        this.enableForm();
+      }else{
+        this.messageClass = 'alert alert-success';
+        this.message = data.message;
+        setTimeout(()=>{
+          this.router.navigate(['/login']);
+        }, 2000)
+      }
+    });
+  }
+
+  checkUsername() {
+    this.authService.checkUsername(this.form.get('username').value).subscribe(data => {
+      if (!data.success) {
+        this.usernameValid = false;
+        this.usernameMessage = data.message;
+      } else {
+        this.usernameValid = true;
+        this.usernameMessage = data.message;
+      }
+    });
+  }
+
+  checkEmail() {
+    this.authService.checkEmail(this.form.get('email').value).subscribe(data => {
+      if (!data.success) {
+        this.emailValid = false;
+        this.emailMessage = data.message;
+      } else {
+        this.emailValid = true;
+        this.emailMessage = data.message;
+      }
+    });
   }
 
   ngOnInit() {
